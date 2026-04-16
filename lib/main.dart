f3ec7248f8dcc97123bcbdca44a8e1590cdcd13d
Luis <luisangelitolopez04@gmail.com>
2026-04-15 21:24:32 -0600

    Name change Main

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gestión de Reportes',
+      title: 'Reporte Ciudadano',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
